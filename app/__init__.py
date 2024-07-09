071cd008504174179f0fdefee3d24e1f314dbd2f
desyasarbini <desya.sarbini@gmail.com>
2024-07-09 16:46:26 +0800

    feat: add cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,16 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask
+from flask_cors import CORS
 from app.routes.men_product_route import men_product_blueprint
 from app.routes.women_product_route import women_product_blueprint
 
 load_dotenv()
 
 app = Flask(__name__)
+
 app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+CORS(app, origins=['http://localhost:3000'], supports_credentials=True)
 
 @app.route("/")
 def helloWorld():
